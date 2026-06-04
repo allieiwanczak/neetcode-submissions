89fb7ade71d58263885edc0bdd9af747ab0e0ce7
Aleksandra Marta Iwanczak <a@iwanczak.com>
2026-06-03 20:48:33 -0400

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> seen;
+        vector<vector<string>> ans;
+        for (const auto& s : strs) {
+            string sortedS = s;
+            sort(sortedS.begin(), sortedS.end());
+            seen[sortedS].push_back(s);
+        }
+        for (auto& x: seen) {
+            ans.push_back(x.second);
+        }
+        return ans;
+    }
+};
