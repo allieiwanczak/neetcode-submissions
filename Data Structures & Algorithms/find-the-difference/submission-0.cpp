ff7f6eefc8ba3026b90227bf9cf55cda6dce0878
Aleksandra Marta Iwanczak <a@iwanczak.com>
2026-06-17 21:14:43 -0400

    Add: find-the-difference - submission-0

---
+++ Data Structures & Algorithms/find-the-difference/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int res = 0;
+        for (char c: s) {
+            res-=c;
+        }
+        for (char c: t) {
+            res+=c;
+        }
+        return (char)res;
+    }
+};
